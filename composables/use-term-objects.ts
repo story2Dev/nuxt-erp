536022ccf09e535f1c4ce84d1774435940f04c94
Mark AI <chanthavong_12@hotmail.com>
2024-01-14 15:46:54 +0700

    improve(ui): add product  tag

--- composables/use-term-objects.ts
+++ composables/use-term-objects.ts
@@ -1,4 +1,4 @@
-import { INSERT_TERM_OBJECTS } from '~/gql/mutate';
+import { DELETE_TERM_OBJECT, INSERT_TERM_OBJECTS } from '~/gql/mutate';
 import type { TermObject, TermObjectInput } from '~/types';
 
 export const useTermObjects = () => {
@@ -18,7 +18,7 @@ export const useTermObjects = () => {
       }
 
       return {
-        termObjects: data?.insertTermObjects as TermObject[],
+        termObjects: data?.insertTermObjects.returning as TermObject[],
         errors,
       };
     } catch (error) {
@@ -26,7 +26,30 @@ export const useTermObjects = () => {
     }
   }
 
+  async function remove(id: string) {
+    try {
+      const { data, errors } = await client.mutate({
+        mutation: DELETE_TERM_OBJECT,
+        variables: {
+          id,
+        },
+      });
+
+      if (errors) {
+        throw new Error(`[useTermObjects]: remove errors: ${errors}`);
+      }
+
+      return {
+        termObject: data?.deleteTermObject as TermObject,
+        errors,
+      };
+    } catch (error) {
+      throw new Error(`[useTermObjects]: remove error: ${error}`);
+    }
+  }
+
   return {
     insert,
+    remove,
   };
 };
